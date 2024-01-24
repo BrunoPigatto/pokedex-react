ed799a037c58c8af9f73cc68f02b3ede79a57ef4
BrunoPigatto <bruunohenrique@hotmail.com>
2024-01-23 21:03:09 -0300

    add: spinner and layoutcontainer components

---
+++ src/components/atoms/container.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 1120px;
+  width: 100%;
+  padding: 0 30px;
+`;
+
+export default function LayoutContainer({ children }) {
+  return <Container>{children}</Container>;
+}
